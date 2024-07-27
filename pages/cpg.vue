e8347d90b4c544e717d51a30bc4e5c8cf65c378f
598523 <598523@egat.co.th>
2024-07-28 04:34:03 +0700

    cpgTable

--- pages/cpg.vue
+++ pages/cpg.vue
@@ -8,9 +8,9 @@
         </CpgSectionhead>
     </LandingContainer>
 
-    <div class="grid gap-10 mx-auto max-w-4xl mt-16">
+    <div class="grid gap-10 mx-auto max-w-4xl mt-12">
         <div>
-
+            <CpgTable />
         </div>
     </div>
 </template>
