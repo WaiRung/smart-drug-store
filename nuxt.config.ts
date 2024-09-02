2de62dbf188eb2ad4a2ddd0f01c70f431739bd87
598523 <598523@egat.co.th>
2024-09-02 11:10:19 +0700

    update packages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -32,6 +32,11 @@ export default defineNuxtConfig({
     pageTransition: {
       name: 'page',
       mode: 'out-in'
+    },
+    head: {
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      ]
     }
   },
 
