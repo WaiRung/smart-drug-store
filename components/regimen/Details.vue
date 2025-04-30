560a73b829d395637f44e42eb2bd2c77957721b1
WaiRung <67456708+WaiRung@users.noreply.github.com>
2025-04-30 11:02:26 +0700

    msdcpgDetail remark ui

--- components/regimen/Details.vue
+++ components/regimen/Details.vue
@@ -14,21 +14,25 @@ const msdcpgDetail = computed(() => {
 })
 
 const filterData = computed(() => {
-    const filter = msdcpgStore.getFilter()
-    return filter
+  const filter = msdcpgStore.getFilter()
+  return filter
 })
 </script>
 
 <template>
   <div class="md:flex justify-between mx-auto max-w-3xl">
 
-    <div class="bg-green-900 px-20 py-2 mt-10 mb-10 mx-auto md:mx-0 max-w-fit rounded-lg flex flex-col items-center text-center">
-      <h5 class="text-white text-3xl md:text-2xl">Patient Group : <span class="text-lime-500">{{ filterData.selectedGroup.val }}</span> </h5>
-      
+    <div
+      class="bg-green-900 px-20 py-2 mt-10 mb-10 mx-auto md:mx-0 max-w-fit rounded-lg flex flex-col items-center text-center">
+      <h5 class="text-white text-3xl md:text-2xl">Patient Group : <span class="text-lime-500">{{
+        filterData.selectedGroup.val }}</span> </h5>
+
     </div>
 
-    <div class="bg-green-900 px-20 py-2 mt-10 mb-10 mx-auto md:mx-0 max-w-fit rounded-lg flex flex-col items-center text-center">
-      <h5 class="text-white text-3xl md:text-2xl">Age : <span class="text-lime-500">{{ filterData.selectedAge.val }}</span></h5>
+    <div
+      class="bg-green-900 px-20 py-2 mt-10 mb-10 mx-auto md:mx-0 max-w-fit rounded-lg flex flex-col items-center text-center">
+      <h5 class="text-white text-3xl md:text-2xl">Age : <span class="text-lime-500">{{ filterData.selectedAge.val
+          }}</span></h5>
     </div>
 
   </div>
@@ -44,17 +48,24 @@ const filterData = computed(() => {
       {{ msdcpgDetail?.ANTIBIOTIC_LABEL }}
     </p>
   </div>
-
-  <h2 v-if="msdcpgDetail?.RM_RX || msdcpgDetail?.RM_DURATION" class="mb-2 text-lg font-semibold">
-    Remarks </h2>
-  <ul class="max-w-md space-y-1 list-disc list-inside">
-    <li v-if="msdcpgDetail?.RM_RX">
-      {{ msdcpgDetail?.RM_RX }}
-    </li>
-    <li v-if="msdcpgDetail?.RM_DURATION">
-      {{ msdcpgDetail?.RM_DURATION }}
-    </li>
-  </ul>
+  <div v-if="msdcpgDetail?.RM_RX || msdcpgDetail?.RM_DURATION"
+    class="bg-green-900 px-20 py-2 my-10 mx-auto max-w-3xl rounded-lg flex flex-col items-center text-center">
+    <h5 class="text-white text-3xl md:text-2xl">Remarks</h5>
+    <ul class="max-w-md space-y-1 list-disc list-inside text-lime-500">
+      <li v-if="msdcpgDetail?.RM_RX">
+        <p class=" mt-4 text-lg md:text-xl">
+          {{ msdcpgDetail?.RM_RX }}
+        </p>
+        
+      </li>
+      <li v-if="msdcpgDetail?.RM_DURATION">
+        <p class=" mt-4 text-lg md:text-xl">
+          {{ msdcpgDetail?.RM_DURATION }}
+        </p>
+        
+      </li>
+    </ul>
+  </div>
 </template>
 
 <style scoped>
