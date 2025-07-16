a63d060c62554f25e5492c305c1daab9c48502f0
WaiRung <67456708+WaiRung@users.noreply.github.com>
2025-07-16 10:31:45 +0700

    get msdcpg.INDICATION_LABEL back

--- stores/msdcpg.ts
+++ stores/msdcpg.ts
@@ -377,6 +377,7 @@ export const useMsdcpgStore = defineStore('useMsdcpgStore', () => {
                     'RM_RX',
                     'RM_DURATION',
 
+                    'INDICATION_LABEL',
                     'ANTIBIOTIC_LABEL',
                 ],
                 pagination: {
@@ -392,7 +393,7 @@ export const useMsdcpgStore = defineStore('useMsdcpgStore', () => {
                     ...msdcpg,
                     DOSE_CHECK: `${msdcpg.DOSE_L || ''}${msdcpg.DOSE_U ? '-' + msdcpg.DOSE_U : ''} ${msdcpg.DOSE_UNIT || ''}${msdcpg.DOSE_LBL || ''} ${msdcpg.DRUG_RM || ''}`,
                     DOSE_M_CHECK: `${msdcpg.DOSE_M || ''} ${msdcpg.DOSE_M_UNIT || ''}${msdcpg.DOSE_M_LBL || ''}`,
-                    INDICATION_LABEL: `${msdcpg.SEVERITY || '-'}${msdcpg.RISK_ORGANISM ? ' / ' + msdcpg.RISK_ORGANISM : ''}`,
+                    // INDICATION_LABEL: `${msdcpg.SEVERITY || '-'}${msdcpg.RISK_ORGANISM ? ' / ' + msdcpg.RISK_ORGANISM : ''}`,
 
                 }))
                 msdcpgs.value = calculatedMmsdcpgs
