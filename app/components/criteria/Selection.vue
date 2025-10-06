db3566eb016c00df5139995b76077df7f904c021
WaiRung <67456708+WaiRung@users.noreply.github.com>
2025-10-06 21:24:42 +0700

    Show NB info for selectedGroup starting with 'NB:'
    Adds a computed property and conditional UI block to display PMA information when the selectedGroup value starts with 'NB:'. This helps users understand the PMA calculation for relevant selections.

--- app/components/criteria/Selection.vue
+++ app/components/criteria/Selection.vue
@@ -136,6 +136,11 @@ const isSearchable = computed(() => {
     return msdcpgStore.isSearchable()
 })
 
+const showNBInfo = computed(() => {
+    const selectedGroup = filterData.value.selectedGroup.val
+    return selectedGroup && selectedGroup.startsWith('NB:')
+})
+
 async function fetchInfectsite() {
     await infectSiteStore.fetchInfectsite()
 }
@@ -417,6 +422,26 @@ async function inputATB_INFO_AE(event) {
             @change="updateGroup"
             @blur="() => clearValidity('selectedGroup')"
         />
+
+        <div v-if="showNBInfo" class="md:flex md:items-center mb-6">
+            <div class="md:w-1/3"></div>
+            <div class="md:w-1/3">
+                <div class="bg-white -mt-4">
+                    <div class="flex">
+                        <div class="flex-shrink-0">
+                            <svg class="h-5 w-3 text-orange-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                                <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd" />
+                            </svg>
+                        </div>
+                        <div class="ml-3">
+                            <p class="text-base text-orange-400">
+                                PMA (Postmenstrual age) = GA + Postnatal age (สัปดาห์)
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 
         <ConditionalSelect
             :field-data="filterData.selectedAge"
